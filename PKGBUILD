8cde40e999a5dde9bca8eefe1fe30353ab0ae14a
Kisbogyi <kisbogyi2003@gmail.com>
2025-07-09 20:46:27 +0200

    change terminal

--- PKGBUILD
+++ PKGBUILD
@@ -5,7 +5,7 @@
 # Contributor: Grigorios Bouzakis <grbzks@gmail.com>
 
 pkgname=dwm_custom
-pkgver=6.6.21
+pkgver=6.6.22
 pkgrel=1
 pkgdesc="A dynamic window manager for X"
 url="https://github.com/Kisbogyi/dwm"
--- config.h
+++ config.h
@@ -34,12 +34,12 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",	NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox",	NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "alacritty",  NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ "kitty",	NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ "st",		NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,		NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+    { "Gimp",	    NULL,     NULL,           0,         1,          0,           0,        -1 },
+    { "Firefox",	NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+    { "kitty",	    NULL,     NULL,           0,         0,          1,           0,        -1 },
+    { NULL,		    NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+    // { "",		NULL,     NULL,           0,         0,          1,           0,        -1 },
+    // { "alacritty",  NULL,     NULL,           0,         0,          1,           0,        -1 },
 };
 
 /* layout(s) */
@@ -71,7 +71,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_light_purple, "-sb", col_dark_purple, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 static const char *lock[]  = { "systemctl", "suspend", NULL };
 static const char *keyboardlayout[]  = { "switchLayout", NULL };
 static const char *screenshot[] = {"flameshot", "launcher", NULL};
